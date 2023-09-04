610ac4116427e5bc7371bd7e81044137532134d2
HOLLY <71253179+hongjinchen@users.noreply.github.com>
2023-09-05 00:30:57 +0800

    增加用户提示弹窗

--- ui-test/src/components/UserGuideDialog.js
+++ ui-test/src/components/UserGuideDialog.js
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@material-ui/core';
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography, List, ListItem } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    // 您可以在此处添加其他样式，如果需要的话
+}));
 
 function UserGuideDialog() {
+    const classes = useStyles();
+
     const [open, setOpen] = useState(true);
 
     const handleClose = () => {
@@ -10,24 +17,51 @@ function UserGuideDialog() {
 
     return (
         <Dialog open={open} onClose={handleClose}>
-            <DialogTitle>Welcome to GWT UI Testing Platform!</DialogTitle>
+            <DialogTitle>
+                <Typography variant="h5" color="primary">
+                    Welcome to GWT UI Testing Platform!
+                </Typography>
+            </DialogTitle>
             <DialogContent>
-                <p>
+                <Typography variant="body1">
                     Here's a quick rundown of what you can do on group space:
-                </p>
-                <ul>
-                    <li><strong>Create a new group:</strong> Click on the card with the '+' icon to start a new team. You can add team details and search for members to add.</li>
-                    <li><strong>Search members:</strong> When creating a team, you can search for users by name. Simply type in the search field and click on the search button.</li>
-                    <li><strong>Manage team members:</strong> When creating a team, you can add or remove members before finalizing the team creation.</li>
-                    <li><strong>View existing groups:</strong> All the teams you're a part of are listed below. Click on any of them to view more details or interact with the team members.</li>
-              
-                </ul>
-                <p>
+                </Typography>
+                <List>
+                    <ListItem>
+                        <Typography variant="body1" color="textSecondary"><strong>Create a new group:</strong></Typography>
+                    </ListItem>
+                    <List>
+                        <ListItem>
+                            <Typography variant="body2">Click on the card with the '+' icon to start a new team. You can add team details and search for members to add. You can only do UI testing if you have created or joined a team.</Typography>
+                        </ListItem>
+                        <ListItem>
+                            <Typography variant="body2">Search members: When creating a team, you can search for users by name. Simply type in the search field and click on the search button.</Typography>
+                        </ListItem>
+                        <ListItem>
+                            <Typography variant="body2">Manage team members: When creating a team, you can add or remove members before finalizing the team creation.</Typography>
+                        </ListItem>
+                    </List>
+                    <ListItem>
+                        <Typography variant="body1" color="textSecondary"><strong>View existing groups:</strong></Typography>
+                    </ListItem>
+                    <ListItem>
+                        <Typography variant="body2">All the teams you're a part of are listed below. Click on any of them to view more details or interact with the team members.</Typography>
+                    </ListItem>
+                    <ListItem>
+                        <Typography variant="body1" color="textSecondary"><strong>View tutorials:</strong></Typography>
+                    </ListItem>
+                    <ListItem>
+                        <Typography variant="body2">We've listed several tutorials for you, from basic HTML to user behaviour guidelines, so once you've found the tutorial you're interested in, just click the "View Tutorial" button.</Typography>
+                    </ListItem>
+                </List>
+                <Typography variant="body1">
                     Enjoy collaborating with your teams!
-                </p>
+                </Typography>
             </DialogContent>
             <DialogActions>
-                <Button onClick={handleClose} color="primary">Got it!</Button>
+                <Button variant="contained" color="primary" onClick={handleClose}>
+                    Got it!
+                </Button>
             </DialogActions>
         </Dialog>
     );
